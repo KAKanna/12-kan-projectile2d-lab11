b09e2ab7d6740a0793524f32dc471f2cfffb50ac
KAKanna <mlg666pop@gmail.com>
2025-04-28 17:15:54 +0700

    Finish

--- Assets/Script/Projectile2D.cs
+++ Assets/Script/Projectile2D.cs
@@ -16,7 +16,20 @@ public class Projectile2D : MonoBehaviour
             {
                 target.transform.position = new Vector2(hit.point.x, hit.point.y);
                 Debug.Log("hit"+hit.collider.name);
+
+                Vector2 projectileVelocity = CalculateProjectileVelocity(shootPoint.position, hit.point, 1f);
+                Rigidbody2D shootBullet = Instantiate(bulletPrefab,shootPoint.position,Quaternion.identity);
+                shootBullet.linearVelocity = projectileVelocity;
             }
         }
+    }
+    Vector2 CalculateProjectileVelocity(Vector2 origin,Vector2 target,float time)
+    {
+        Vector2 distance = target - origin;
+        float velocityX = distance.x / time;
+        float velocityY = distance.y / time + 0.5f * Mathf.Abs(Physics2D.gravity.y) * time;
+
+        Vector2 projectileVeclocity = new Vector2(velocityX, velocityY);
+        return projectileVeclocity;
     }
 }
